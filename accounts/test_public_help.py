bf2fe3348428f5edfc0725704fcd69e274b02e14
kilerep <kilerepp@gmail.com>
2026-06-30 20:04:53 -0300

    test(ajuda): acessa rota pública por HTTPS na validação

--- accounts/test_public_help.py
+++ accounts/test_public_help.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 class PublicHelpPageTests(SimpleTestCase):
     def test_help_page_explains_current_mei_first_flow(self):
-        response = self.client.get(reverse("help"))
+        response = self.client.get(reverse("help"), secure=True)
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "MEIs e prestadores")
@@ -15,7 +15,8 @@ class PublicHelpPageTests(SimpleTestCase):
         self.assertContains(response, "Meu Perfil → Segurança da conta")
 
     def test_help_page_does_not_keep_outdated_company_first_message(self):
-        response = self.client.get(reverse("help"))
+        response = self.client.get(reverse("help"), secure=True)
 
+        self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, "Empresa cadastra o MEI")
         self.assertNotContains(response, "Empresa acompanha em tempo real")
